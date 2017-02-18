9e061766f9080b08adc83c29338e5ab669067117
Eli <elimill12345@hotmail.com>
2017-02-17 17:33:47 -0800

    add annotation grabbing to patient_splitter

--- patient_splitter.py
+++ patient_splitter.py
@@ -3,6 +3,7 @@ import sys
 import re
 from collections import defaultdict
 from record import Record
+from annotation_matcher import get_annotation
 
 '''
 Patient Splitter Method
@@ -16,6 +17,12 @@ This doesn't promise a particular order for the patients. Is there any reason we
 rx_tagged_section = re.compile("<(\\w+)>(.+?)</\\1>", re.DOTALL)
 
 def get_file_list(dir):
+    # this stuff throws an exception for some reason. It should get figured out
+    # dir = re.sub(r"\\|/", os.sep, dir)
+    # dir = dir.strip(os.sep)
+    # print([os.sep.join((dir, f)) for f in os.listdir(dir) if f.endswith(".txt")])
+    # return [os.sep.join((dir, f)) for f in os.listdir(dir) if f.endswith(".txt")]
+
     dir = dir.strip("/")
     return ["/".join((dir, f)) for f in os.listdir(dir) if f.endswith(".txt")]
 
@@ -28,7 +35,7 @@ def get_patient_id(record):
     else:
         return first_section.group(2).strip("\n")
 
-def get_records(file_list):
+def get_records(file_list, test):
     all_patients = []
 
     for file_name in file_list:
@@ -40,8 +47,13 @@ def get_records(file_list):
             _id = get_patient_id(record)
             file_records[_id] += record
 
+        # get the name of the annotations file corresponding to this record file
+        file_version = "train" if not test else "test"
+        file_id = "_".join(file_name.split(os.sep)[-1].split("_")[1:3])
+        annot_file_name = "/".join(file_name.split("/")[:-2] + ["Annotations"] + ["annotations_" + file_id + "_" + file_version + ".json"])
+
         for _id in file_records.keys():
-            record = Record(_id, file_records[_id], file=file_name)
+            record = Record(_id, file_records[_id], file_name, get_annotation(_id, annot_file_name))
             all_patients.append(record)
 
     # Array of all patient strings. May be multiple records for each patient
@@ -74,14 +86,14 @@ def get_patients(dir):
     '''
     return get_records(get_file_list(dir))
 
-def load_records(dir):
-    return get_records(get_file_list(dir))
+def load_records(dir, test=False):
+    return get_records(get_file_list(dir), test)
 
 if __name__ == "__main__":
     files = get_file_list(sys.argv[1])
     # List of Patient strings. The method splits on the opening line,
     # so "**PROTECTED[begin]" is deleted. It'd be easy enough to add back in if necessary
-    records = get_records(files)
+    records = get_records(files, False)
     with open(sys.argv[2], "w") as out_file:
         print(records[0].record)
         for record in records:
