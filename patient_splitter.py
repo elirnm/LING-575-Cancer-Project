660ca0394d1db83fae46cd7971efb859d857b040
kwtkns <kwtkns@uw.edu>
2017-02-22 15:37:04 -0800

    Split on Record, not Patient

--- patient_splitter.py
+++ patient_splitter.py
@@ -14,21 +14,28 @@ Usage: python3 recordSplitter.py dir_name output_file
 
 This doesn't promise a particular order for the patients. Is there any reason we'd care?
 '''
-rx_tagged_section = re.compile("<(\\w+)>(.+?)</\\1>", re.DOTALL)
+#rx_tagged_section = re.compile("<(\\w+)>(.+?)</\\1>", re.DOTALL)
+rx_patient_id = re.compile("<(PATIENT_DISPLAY_ID)>(.+?)</PATIENT_DISPLAY_ID>", re.DOTALL)
+rx_record_id = re.compile("<(RECORD_DOCUMENT_ID)>(.+?)</RECORD_DOCUMENT_ID>", re.DOTALL)
 
 def get_file_list(dir):
     dir = re.sub(r"\\|/", re.escape(os.sep), dir)
     dir = dir.strip(os.sep)
     return [os.sep.join((dir, f)) for f in os.listdir(dir) if f.endswith(".txt")]
 
-def get_patient_id(record):
-    first_section = rx_tagged_section.search(record)
-    if not first_section or first_section.group(1) != "PATIENT_DISPLAY_ID":
-        sys.stderr.write("No Patient ID found:\n")
-        sys.stderr.write(record)
-        return "none"
-    else:
-        return first_section.group(2).strip("\n")
+def get_record_id(record):
+    patient_id_match = rx_patient_id.search(record)
+    record_id_match = rx_record_id.search(record)
+    if record_id_match:
+        id = record_id_match.group(2).strip("\n")
+        if id != "":
+            return id
+    if patient_id_match:
+        id = patient_id_match.group(2).strip("\n")
+        if id != "":
+            return(id)
+
+    sys.stderr.write("No Id Found for record: " + record[0:160]  + "\n")
 
 def get_records(file_list, test):
     all_patients = []
@@ -36,10 +43,12 @@ def get_records(file_list, test):
     for file_name in file_list:
         records = split_records(file_name)
 
-        # check for records with matching patient IDs
+        # Get Record ID. (May be PAT or REC)
         file_records = defaultdict(str)
         for record in records:
-            _id = get_patient_id(record)
+            _id = get_record_id(record)
+            if _id in file_records:
+                sys.stderr.write("Duplicate record ID: " + _id + "\n")
             file_records[_id] += record
 
         # get the name of the annotations file corresponding to this record file
@@ -62,16 +71,6 @@ def split_records(file_name):
 
     records = text.split("**PROTECTED[begin]")
     return records[1:] # The first record is "DUMMY " Don't return that one
-
-# For testing
-def print_patient_IDs(patients):
-    for patient in patients:
-        first_section = rx_tagged_section.search(patient.record)
-        if not first_section:
-            sys.stderr.write("Couldn't find patient ID\n")
-            print(patient)
-        else:
-            print(first_section.group(2).strip("\n"))
 
 def get_patients(dir):
     '''
